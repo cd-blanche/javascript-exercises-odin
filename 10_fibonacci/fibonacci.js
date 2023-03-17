1ddbdcb24500c49ed49ef36bb2e31cbd4491defb
blanche <79345528+cd-blanche@users.noreply.github.com>
2023-03-16 21:05:55 -0400

    start exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  // compute fibonacci sequence up until num
+  // what i need
+  // the starting value 0 and 1
+  // the two previous values
+  // the sum of the previous two values
 
+  const fibonacci = [0, 1];
+  // return num member of the series
+  return fibonacci[num -1];
 };
 
 // Do not edit below this line
